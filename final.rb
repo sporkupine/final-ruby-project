6201a49a6cd1dbd2ef930a25a9db8bcc4be452d5
sporkupine <coffeechief25@gmail.com>
2022-02-28 16:12:37 -0600

    define create hash digest method

--- final.rb
+++ final.rb
@@ -1,14 +1,7 @@
 require 'bcrypt'
 
-my_password = BCrypt::Password.create("MyPassword")
 
-puts "password: #{my_password}"
-puts "version: #{my_password.version}"
-puts "cost: #{my_password.cost}"
-puts my_password == "MyPassword"
-puts my_password == "somethingelse"
+def create_hash_digest(password)
+  BCrypt::Password.create(password)
+end
 
-my_password = BCrypt::Password.new("$2a$12$ei62cKw.e9L.uUgH2KhE5uaSCkMbXqJwhDZWs0kQ/27qZif8sX25e")
-
-puts my_password == "MyPassword"
-puts my_password == "NotMyPassword"
